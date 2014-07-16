198dd3bfd8b116e3f49c2cfec48047b824a5d5d2
seanahrens <seanahrens@gmail.com>
2014-07-16 00:47:01 -0400

    account deletion now working

--- config/routes.rb
+++ config/routes.rb
@@ -28,40 +28,40 @@ Rails.application.routes.draw do
   get 'terms' => 'pages#terms'
 
 
+devise_for :user
+# # Authentication
+#   devise_for :user, skip: [:sessions, :passwords, :confirmations, :registrations]
+#   as :user do
+#     # session handling
+#     get     '/login'  => 'devise/sessions#new',     as: 'new_user_session'
+#     post    '/login'  => 'devise/sessions#create',  as: 'user_session'
+#     delete  '/logout' => 'devise/sessions#destroy', as: 'destroy_user_session'
 
-# Authentication
-  devise_for :user, skip: [:sessions, :passwords, :confirmations, :registrations]
-  as :user do
-    # session handling
-    get     '/login'  => 'devise/sessions#new',     as: 'new_user_session'
-    post    '/login'  => 'devise/sessions#create',  as: 'user_session'
-    delete  '/logout' => 'devise/sessions#destroy', as: 'destroy_user_session'
+#     # joining
+#     # get   '/join' => 'devise/registrations#new',    as: 'new_user_registration'
+#     # post  '/join' => 'devise/registrations#create', as: 'user_registration'
 
-    # joining
-    get   '/join' => 'devise/registrations#new',    as: 'new_user_registration'
-    post  '/join' => 'devise/registrations#create', as: 'user_registration'
+#     # scope '/account' do
+#     #   # password reset
+#     #   get   '/reset-password'        => 'devise/passwords#new',    as: 'new_user_password'
+#     #   put   '/reset-password'        => 'devise/passwords#update', as: 'user_password'
+#     #   post  '/reset-password'        => 'devise/passwords#create'
+#     #   get   '/reset-password/change' => 'devise/passwords#edit',   as: 'edit_user_password'
 
-    scope '/account' do
-      # password reset
-      get   '/reset-password'        => 'devise/passwords#new',    as: 'new_user_password'
-      put   '/reset-password'        => 'devise/passwords#update', as: 'user_password'
-      post  '/reset-password'        => 'devise/passwords#create'
-      get   '/reset-password/change' => 'devise/passwords#edit',   as: 'edit_user_password'
+#     #   # confirmation
+#     #   get   '/confirm'        => 'devise/confirmations#show',   as: 'user_confirmation'
+#     #   post  '/confirm'        => 'devise/confirmations#create'
+#     #   get   '/confirm/resend' => 'devise/confirmations#new',    as: 'new_user_confirmation'
 
-      # confirmation
-      get   '/confirm'        => 'devise/confirmations#show',   as: 'user_confirmation'
-      post  '/confirm'        => 'devise/confirmations#create'
-      get   '/confirm/resend' => 'devise/confirmations#new',    as: 'new_user_confirmation'
+#     #   # settings & cancellation
+#     #   get '/cancel'   => 'devise/registrations#cancel', as: 'cancel_user_registration'
+#     #   get '/settings' => 'devise/registrations#edit',   as: 'edit_user_registration'
+#     #   put '/settings' => 'devise/registrations#update'
 
-      # settings & cancellation
-      get '/cancel'   => 'devise/registrations#cancel', as: 'cancel_user_registration'
-      get '/settings' => 'devise/registrations#edit',   as: 'edit_user_registration'
-      put '/settings' => 'devise/registrations#update'
-
-      # account deletion
-      delete '' => 'devise/registrations#destroy'
-    end
-  end
+#     #   # account deletion
+#     #   delete '' => 'devise/registrations#destroy', as: 'destroy_user_registration'
+#     # end
+#   end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
