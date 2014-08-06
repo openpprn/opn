3c4114cf3e7fce75d715609cac0ea5a8023e9ce2
seanahrens <seanahrens@gmail.com>
2014-08-06 09:38:42 -0400

    fixed after sign in bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || research_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || research_karma_path
   end
 
 
