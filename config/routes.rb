8ba0690a3eb8d96ae3053d5f5e07372ac8fc7c54
seanahrens <seanahrens@gmail.com>
2014-08-11 18:17:14 -0400

    added uservoice and google analytics menu items to admin

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,8 @@ Rails.application.routes.draw do
   get 'admin_notifications' => 'admin#admin_notifications'
   get 'admin_research_topics' => 'admin#admin_research_topics'
 
+
+
   match 'add_role', to: "admin#add_role_to_user", via: :post, as: :add_role
   match 'remove_role', to: "admin#remove_role_from_user", via: :post, as: :remove_role
 
