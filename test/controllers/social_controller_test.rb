f4418714ffa36648ee1345e0e88411f980b1779f
seanahrens <seanahrens@gmail.com>
2014-08-13 21:53:45 -0400

    moved survey tests into health_data_controller_tests, where they belong

--- test/controllers/social_controller_test.rb
+++ test/controllers/social_controller_test.rb
@@ -52,19 +52,4 @@ class SocialControllerTest < ActionController::TestCase
   end
 
 
-
-  test "User needs to be logged in to see surveys" do
-    get :surveys
-
-    assert_response 302
-  end
-
-  test "User can see a list of unstarted surveys" do
-    login(users(:social))
-
-    get :surveys
-
-    assert_response :success
-  end
-
 end
