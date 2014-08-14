88394c09f54d221ad37a5394416fc0b029724588
seanahrens <seanahrens@gmail.com>
2014-08-13 21:03:10 -0400

    fix to survey controller test

--- test/controllers/surveys_controller_test.rb
+++ test/controllers/surveys_controller_test.rb
@@ -50,4 +50,19 @@ class SurveysControllerTest < ActionController::TestCase
 
     assert_response :success
   end
+
+  test "User needs to be logged in to see surveys" do
+    get :surveys
+
+    assert_response 302
+  end
+
+  test "User can see a list of unstarted surveys" do
+    login(users(:social))
+
+    get :surveys
+
+    assert_response :success
+  end
+
 end
