42650ad76ff47f3b9a3f80a42f7c8c59a1165592
Piotr Mankowski <pmankowski@partners.org>
2014-08-17 17:26:05 -0400

    Refactored myapnea styles. Finished home page redesign - Closed #7.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,11 @@
 class StaticController < ApplicationController
 
+  def home
+    @pc = YAML.load_file(Rails.root.join('lib', 'data', 'content', "home.#{I18n.locale}.yml"))['en']['home']
+
+    @research_qs = Group.find_by_name("Research Questions").questions.sort_by{|q| -q.rating }
+  end
+
   def privacy_policy
     @pc = YAML.load_file(Rails.root.join('lib', 'data', 'content', "privacy_policy.#{I18n.locale}.yml"))
   end
