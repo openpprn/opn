3adcbf1e1ea0a54ebb0903af474af416c4084189
Piotr Mankowski <pmankowski@partners.org>
2014-08-17 20:44:42 -0400

    Re-designed the Learn page. Closed myapnea/www_myapnea_org#4

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,9 +1,13 @@
 class StaticController < ApplicationController
 
   def home
-    @pc = YAML.load_file(Rails.root.join('lib', 'data', 'content', "home.#{I18n.locale}.yml"))['en']['home']
+    @pc = page_content('home')#YAML.load_file(Rails.root.join('lib', 'data', 'content', "home.#{I18n.locale}.yml"))['en']['home']
 
     @research_qs = Group.find_by_name("Research Questions").questions.sort_by{|q| -q.rating }
+  end
+
+  def learn
+    @pc = page_content('learn') #.load_file(Rails.root.join('lib', 'data', 'content', "learn.#{I18n.locale}.yml"))['en']['learn']
   end
 
   def privacy_policy
@@ -14,4 +18,11 @@ class StaticController < ApplicationController
     render layout: "theme"
   end
 
+
+  private
+
+  def page_content(name)
+    YAML.load_file(Rails.root.join('lib', 'data', 'content', "#{name}.#{I18n.locale}.yml"))[I18n.locale.to_s][name]
+  end
 end
+
