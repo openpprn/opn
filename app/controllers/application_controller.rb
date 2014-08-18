a436cb8ea7caf777fb8efa7e78bbada89068fad3
Piotr Mankowski <pmankowski@partners.org>
2014-08-18 09:56:23 -0400

    Fixed post sign-in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || research_karma_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || user_dashboard_path
   end
 
 
