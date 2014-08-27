b9b05a5572fa78f3849935caee57896103118378
Remo Mueller <remosm@gmail.com>
2014-08-27 11:46:25 -0400

    Use rails convention for development and test database names, and fixed an incorrect/duplicated migration

--- db/migrate/20140818024723_add_my_apnea_fields_to_user.rb
+++ db/migrate/20140818024723_add_my_apnea_fields_to_user.rb
@@ -2,7 +2,7 @@ class AddMyApneaFieldsToUser < ActiveRecord::Migration
   def change
     add_column :users, :year_of_birth, :integer
     add_column :users, :zip_code, :string
-    add_column :users, :accepted_consent_at, :timestamp
+    # add_column :users, :accepted_consent_at, :timestamp # This is already in an earlier migration
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
 
