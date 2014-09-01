5da9c290f1f07903413c1723b498d48dc1e0e19a
Piotr Mankowski <pmankowski@partners.org>
2014-08-31 22:11:00 -0400

    Fixed survey bug. Worked on notifications and blog posts - views and forms

--- app/models/surveys/answer_value.rb
+++ app/models/surveys/answer_value.rb
@@ -13,7 +13,7 @@ class AnswerValue < ActiveRecord::Base
 
   def show_value
     if answer_template.data_type == 'answer_option_id'
-      answer_option.value
+      answer_option.present? ? answer_option.value : nil
     else
       value
     end
