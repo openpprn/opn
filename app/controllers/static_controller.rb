39ec3da7a0bb8ef1cb669b0856cea40a619e9d35
seanahrens <seanahrens@gmail.com>
2014-09-09 16:04:22 +0700

    added ccfa partners resource pages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,3 +1,8 @@
 class StaticController < ApplicationController
 
+  def content
+    @page = params[:page]
+    render "/static/content/#{@page}", :layout => "content"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
   get 'about' => 'static#about'
   get 'external_link_warning' => 'static#external_link_warning'
   get 'terms' => 'static#terms'
+  #Content Pages
+  get 'content/:page' => 'static#content'
+
 
 
   # Research Section
