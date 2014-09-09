562e109857e08190d4b164d97c88f3bf298e7574
seanahrens <seanahrens@gmail.com>
2014-09-09 13:42:35 +0700

    added CCFA specific uservoice and google analytics keys

--- config/initializers/pprn.rb
+++ config/initializers/pprn.rb
@@ -1,4 +1,4 @@
 #Load the PPRN specific data
 PPRNS = YAML.load_file("config/pprn.yml")
-Rails.application.config.uservoice_api_key = nil
-Rails.application.config.google_analytics_web_property_id = nil # eg. "UA-XXXX-1"
+Rails.application.config.uservoice_api_key = "H39t0NrsoAK7oVfEnDrkQ"
+Rails.application.config.google_analytics_web_property_id = "UA-53742602-1" # eg. "UA-XXXX-1"
