6a6760ef88f87bdc34b7a1bd62ba81252f3d2d5b
pmanko <pmankowski@partners.org>
2014-09-17 14:46:51 -0400

    Refactored google analytics script

--- lib/initial_setup.rb
+++ lib/initial_setup.rb
@@ -12,7 +12,8 @@ puts root_folder
 
 files = [['config/environments', 'production.rb'],
   ['config/initializers', '00_site_name.rb'],
-  ['config/initializers', 'action_mailer.rb']
+  ['config/initializers', 'action_mailer.rb'],
+  ['app/assets/javascripts', 'google_analytics.js']
   # ['config','database.yml'],
   # ['config/initializers', 'secret_token.rb'],
   # ['config/initializers', 'devise.rb']
