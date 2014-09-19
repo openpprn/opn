7a199c8fa91c682ef3edbf3d786a5616314244e5
Piotr Mankowski <pmankowski@partners.org>
2014-09-19 19:48:05 -0400

    Added testing for Blogs and Posts. Improved test coverage

---
+++ test/controllers/blog_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class PostControllerTest < ActionController::TestCase
+  test "Moderator can create a new blog post" do
+
+  end
+
+  test "Moderator can create a new notification" do
+
+  end
+
+  test "Non-moderators cannot create a new blog post or notification" do
+
+  end
+
+  test "Moderator can delete post" do
+
+  end
+
+  test "Moderator can update post" do
+
+  end
+
+
+end
