7d86a512e1868c8b979b8cf1c0b65922f0ef0942
pmanko <pmankowski@partners.org>
2014-09-19 14:15:40 -0400

    Fixed failing tests

--- test/controllers/surveys_controller_test.rb
+++ test/controllers/surveys_controller_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper.rb'
 
 class SurveysControllerTest < ActionController::TestCase
-  setup do
-    add_edges
-  end
-
   test "User can start a survey" do
     login(users(:social))
 
@@ -29,26 +25,17 @@ class SurveysControllerTest < ActionController::TestCase
   end
 
   test "User can answer question on survey" do
-    login(users(:social))
+    login(users(:has_incomplete_survey))
 
-    get :start_survey, question_flow_id: question_flows(:survey_1).id
-
-    assert assigns(:answer_session)
-    assert_response :success
-
-    post :process_answer, { 'question_id' => question_flows(:survey_1).first_question.id, 'answer_session_id' => assigns(:answer_session).id, question_flows(:survey_1).first_question.id.to_s => "blue", "direction" => "next"}
+    post :process_answer, { 'question_id' => questions(:q2a).id, 'answer_session_id' => answer_sessions(:incomplete).id,  questions(:q2a).id.to_s => "blue", "direction" => "next"}
 
     assert_redirected_to survey_report_path(assigns(:answer_session))
   end
 
   test "User can view survey report" do
-    login(users(:social))
+    login(users(:has_completed_survey))
 
-    get :start_survey, question_flow_id: question_flows(:survey_1).id
-    as_id = assigns(:answer_session).id
-    post :process_answer, { 'question_id' => question_flows(:survey_1).first_question.id, 'answer_session_id' => assigns(:answer_session).id, question_flows(:survey_1).first_question.id.to_s => "blue", "direction" => "next"}
-
-    get :show_report, answer_session_id: as_id
+    get :show_report, answer_session_id: answer_sessions(:complete)
 
     assert_response :success
   end
