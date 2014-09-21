0ecac4a36a5968f3d6fbbbb15a3784e9adc97391
seanahrens <seanahrens@gmail.com>
2014-09-21 15:20:36 +0800

    fixed spelling error in filename of sepArate_research_topic_model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140919150504) do
+ActiveRecord::Schema.define(version: 20140919235357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,12 +86,13 @@ ActiveRecord::Schema.define(version: 20140919150504) do
   end
 
   create_table "comments", force: true do |t|
-    t.text     "body",       null: false
+    t.text     "body",              null: false
     t.string   "state"
     t.integer  "user_id"
-    t.integer  "post_id",    null: false
+    t.integer  "post_id",           null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "research_topic_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
@@ -260,6 +261,12 @@ ActiveRecord::Schema.define(version: 20140919150504) do
     t.datetime "updated_at"
   end
 
+  create_table "research_topics", force: true do |t|
+    t.string "text"
+    t.text   "description"
+    t.string "state",       default: "under_review", null: false
+  end
+
   create_table "roles", force: true do |t|
     t.string   "name"
     t.integer  "resource_id"
@@ -356,6 +363,9 @@ ActiveRecord::Schema.define(version: 20140919150504) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "label"
+    t.integer  "research_topic_id"
+    t.integer  "comment_id"
+    t.integer  "post_id"
   end
 
 end
