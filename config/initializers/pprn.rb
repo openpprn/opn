9600105ed1a25eef02968c6e3b0828becf5f7645
seanahrens <seanahrens@gmail.com>
2014-10-13 14:38:13 +0700

    updated readme to include now necessary 'figaro install' command plus updating application.yml with uservoice and google analytics keys

--- config/initializers/pprn.rb
+++ config/initializers/pprn.rb
@@ -1,7 +1,12 @@
-#Load the PPRN specific data
-DUMMY_HEALTH_DATA = YAML.load_file("config/health_data.yml")
-DUMMY_DATA_SOURCES = DUMMY_HEALTH_DATA["data_sources"]
-DUMMY_DATA_SOURCE_TYPES = DUMMY_HEALTH_DATA["data_source_types"]
+#####################
+# The title of your network and the conditions it helps
+#####################
+PPRN = "ccfa"
+PPRN_TITLE = "CCFA Partners"
+PPRN_CONDITION = "Crohn's & Ulcerative Colitis"
+PPRN_CONDITIONS = ["Crohn's", "Ulcerative Colitis", "Indeterminate Colitis"]
+PPRN_SUPPORT_EMAIL = "support@partners.ccfa.org"
+
 
 #####################
 # External Accounts
@@ -9,11 +14,11 @@ DUMMY_DATA_SOURCE_TYPES = DUMMY_HEALTH_DATA["data_source_types"]
 OODT_ENABLED = true
 VALIDIC_ENABLED = true
 
-#####################
-# The title of your network and the conditions it helps
-#####################
-PPRN = "ccfa"
-PPRN_TITLE = "CCFA Partners"
-PPRN_CONDITION = "Crohn's & Ulcerative Colitis"
-CONDITIONS = ["Crohn's", "Ulcerative Colitis", "Indeterminate Colitis"]
-SUPPORT_EMAIL = "support@partners.ccfa.org"
+
+# Load dummy health data for demonstration
+DUMMY_HEALTH_DATA = YAML.load_file("config/health_data.yml")
+DUMMY_DATA_SOURCES = DUMMY_HEALTH_DATA["data_sources"]
+DUMMY_DATA_SOURCE_TYPES = DUMMY_HEALTH_DATA["data_source_types"]
+
+
+
