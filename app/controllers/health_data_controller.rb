ebcb05bd1e6e15996f000128c96eb087ff865013
seanahrens <seanahrens@gmail.com>
2014-12-23 16:58:06 +0700

    added missing current_user check to health check_in partial on health_data page. was crashing page when user wasn't logged in

--- app/controllers/health_data_controller.rb
+++ app/controllers/health_data_controller.rb
@@ -41,12 +41,14 @@ class HealthDataController < ApplicationController
   private
 
   def check_in_setup
-    @question_flow = QuestionFlow.find_by_name_en("Daily Trends")
-    @answer_session = AnswerSession.most_recent(@question_flow.id, current_user.id)
+    if current_user
+      @question_flow = QuestionFlow.find_by_name_en("Daily Trends")
+      @answer_session = AnswerSession.most_recent(@question_flow.id, current_user.id)
 
-    if @answer_session.nil? or (@answer_session.completed? and (Time.zone.now - @answer_session.updated_at) >= Figaro.env.check_in_frequency.to_i * 3600) or params[:new_check_in]
-      #raise StandardError
-      @answer_session = AnswerSession.create(user_id: current_user.id, question_flow_id: @question_flow.id)
+      if @answer_session.nil? or (@answer_session.completed? and (Time.zone.now - @answer_session.updated_at) >= Figaro.env.check_in_frequency.to_i * 3600) or params[:new_check_in]
+        #raise StandardError
+        @answer_session = AnswerSession.create(user_id: current_user.id, question_flow_id: @question_flow.id)
+      end
     end
 
   end
